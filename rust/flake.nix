a796442df92a486e60bb51c01117568d1748df4c
aruZeta <me@aruzeta.com>
2023-03-07 20:57:22 +0100

    [add] rust / hello world

---
+++ rust/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake with rust";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/c90dcc73273387b152e7d3cf1a50abec00c3204b";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.rustc;
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+        ];
+      };
+    };
+}
