090b690a788580dd62afbf14e2ff9aab3a8e1423
aruZeta <me@aruzeta.com>
2023-03-19 11:25:17 +0100

    [add] rust / allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,39 @@
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Allergen {
+    Eggs = 1 << 0,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Allergies { score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        let allergen = *allergen as u32;
+        return self.score & allergen == allergen;
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        return vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ].into_iter().filter(|allergen| self.is_allergic_to(allergen)).collect();
+    }
+}
